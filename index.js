7ae4ab525d6e511909ab6eaf1ff8fed0571344a9
Wahu Ngugi <ngugisandralucy@gmail.com>
2026-05-30 20:37:54 +0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const app = require("./app");
-const { env } = require("./config/env");
-
-app.listen(env.port, () => {
-  console.log(`Safari AI backend listening on http://localhost:${env.port}`);
-});
